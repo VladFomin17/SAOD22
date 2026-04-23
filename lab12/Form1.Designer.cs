5035fe7b33032994a993c9f6615c01de89b1955d
Vlad <fominvladosik@gmail.com>
2026-04-23 20:44:57 +0300

    Добавьте файлы проекта.

---
+++ lab12/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SAOD22
+{
+    partial class SortingForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.arraySize = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.opPercent = new System.Windows.Forms.NumericUpDown();
+            this.Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SortedHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arraySize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opPercent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column5,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.SortedHeader});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 14);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(1067, 178);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // arraySize
+            // 
+            this.arraySize.Location = new System.Drawing.Point(362, 210);
+            this.arraySize.Margin = new System.Windows.Forms.Padding(4);
+            this.arraySize.Maximum = new decimal(new int[] {
+            1316134912,
+            2328,
+            0,
+            0});
+            this.arraySize.Name = "arraySize";
+            this.arraySize.Size = new System.Drawing.Size(171, 22);
+            this.arraySize.TabIndex = 1;
+            this.arraySize.Value = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 212);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Размер массива";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 210);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сортировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.onSortClick);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::SAOD22.Properties.Resources.icons8_exit_button_50;
+            this.button2.Location = new System.Drawing.Point(1007, 212);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 68);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(668, 212);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "%ОП";
+            // 
+            // opPercent
+            // 
+            this.opPercent.Location = new System.Drawing.Point(715, 210);
+            this.opPercent.Margin = new System.Windows.Forms.Padding(4);
+            this.opPercent.Name = "opPercent";
+            this.opPercent.Size = new System.Drawing.Size(171, 22);
+            this.opPercent.TabIndex = 5;
+            this.opPercent.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Сравнения";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Присвоения";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Время";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.Width = 125;
+            // 
+            // SortedHeader
+            // 
+            this.SortedHeader.HeaderText = "Отсортировано?";
+            this.SortedHeader.MinimumWidth = 6;
+            this.SortedHeader.Name = "SortedHeader";
+            this.SortedHeader.Width = 125;
+            // 
+            // SortingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 322);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.opPercent);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.arraySize);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "SortingForm";
+            this.Text = "Фомин 24ВП1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arraySize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opPercent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown arraySize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown opPercent;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SortedHeader;
+    }
+}
+
---
+++ lab12/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace SAOD22
+{
+    public partial class SortingForm : Form
+    {
+        public SortingForm()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 6;
+            dataGridView1.Rows[0].Cells[1].Value = "Простое 2ф";
+            dataGridView1.Rows[1].Cells[1].Value = "Простое 1ф";
+            dataGridView1.Rows[2].Cells[1].Value = "Естественное 2ф";
+            dataGridView1.Rows[3].Cells[1].Value = "Естественное 1ф";
+            dataGridView1.Rows[4].Cells[1].Value = "Поглощение";
+
+            dataGridView1.Rows[0].Cells[0].Value = true;
+        }
+
+        bool IsSorted(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+                if (a[i] > a[i + 1])
+                    return false;
+            return true;
+        }
+
+        private int[] TwoPhaseMergeSort(int[] a, int seriesLength, ref int comparisons, ref int assignments)
+        {
+            int seriesCount = (int)Math.Ceiling(a.Length / (double)seriesLength);
+
+            int bLength = 0, cLength = 0, aLength = a.Length;
+            while (aLength > 0)
+            {
+                if (aLength < seriesLength)
+                {
+                    bLength += aLength;
+                    break;
+                }
+
+                bLength += seriesLength;
+                aLength -= seriesLength * 2;
+            }
+
+            cLength = a.Length - bLength;
+
+            int[] b = new int[bLength];
+            int[] c = new int[cLength];
+
+            int bIndex = 0, cIndex = 0;
+            for (int i = 0; i < seriesCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < seriesLength && i * seriesLength + j < a.Length; j++)
+                    {
+                        b[bIndex++] = a[i * seriesLength + j];
+                        assignments++;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < seriesLength && i * seriesLength + j < a.Length; j++)
+                    {
+                        c[cIndex++] = a[i * seriesLength + j];
+                        assignments++;
+                    }
+                }
+            }
+
+            a = TwoInOneMerge(a, b, c, seriesLength, ref comparisons, ref assignments);
+
+            seriesLength *= 2;
+            if (seriesLength < a.Length)
+                a = TwoPhaseMergeSort(a, seriesLength, ref comparisons, ref assignments);
+
+            return a;
+        }
+
+        private int[] TwoInOneMerge(int[] a, int[] b, int[] c, int seriesLength, ref int comparisons, ref int assignments)
+        {
+            int bSeriesCount = (int)Math.Ceiling(b.Length / (double)seriesLength);
+            int cSeriesCount = (int)Math.Ceiling(c.Length / (double)seriesLength);
+
+            int i = 0, j = 0;
+            int aIndex = 0;
+
+            for (int k = 0; k < Math.Max(bSeriesCount, cSeriesCount); k++)
+            {
+                int bEnd = Math.Min(i + seriesLength, b.Length);
+                int cEnd = Math.Min(j + seriesLength, c.Length);
+                Merge(ref i, ref j, bEnd, cEnd, b, c, a, ref aIndex, ref comparisons, ref assignments);
+            }
+
+            return a;
+        }
+
+        private void Merge(ref int i, ref int j, int bEnd, int cEnd, int[] b, int[] c, int[] a, ref int aIndex, ref int comparisons, ref int assignments)
+        {
+            while (i < bEnd && j < cEnd)
+            {
+                comparisons++;
+
+                if (b[i] < c[j])
+                {
+                    a[aIndex++] = b[i++];
+                    assignments++;
+                }
+                else
+                {
+                    a[aIndex++] = c[j++];
+                    assignments++;
+                }
+            }
+
+            while (i < bEnd)
+            {
+                a[aIndex++] = b[i++];
+                assignments++;
+            }
+
+            while (j < cEnd)
+            {
+                a[aIndex++] = c[j++];
+                assignments++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void onSortClick(object sender, EventArgs e)
+        {
+            int n = (int)arraySize.Value;
+            Random rnd = new Random();
+
+            int[] source = new int[n];
+            for (int i = 0; i < n; i++)
+                source[i] = rnd.Next(n);
+
+            int comparisons, assignments;
+
+            if (Convert.ToBoolean(dataGridView1.Rows[0].Cells[0].Value))
+            {
+                int[] sortingArray = (int[])source.Clone();
+                comparisons = 0;
+                assignments = 0;
+
+                int t1 = Environment.TickCount;
+                sortingArray = TwoPhaseMergeSort(sortingArray, 1, ref comparisons, ref assignments);
+                int time = Environment.TickCount - t1;
+
+                dataGridView1.Rows[0].Cells[2].Value = comparisons;
+                dataGridView1.Rows[0].Cells[3].Value = assignments;
+                dataGridView1.Rows[0].Cells[4].Value = time;
+                dataGridView1.Rows[0].Cells[5].Value = IsSorted(sortingArray) ? "Да" : "Нет";
+            }
+            else
+            {
+                dataGridView1.Rows[0].Cells[2].Value = "";
+                dataGridView1.Rows[0].Cells[3].Value = "";
+                dataGridView1.Rows[0].Cells[4].Value = "";
+                dataGridView1.Rows[0].Cells[5].Value = "";
+            }
+
+            
+        }
+    }
+}
